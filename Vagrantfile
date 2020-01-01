6e269685c0541a04cfec431fd15a0a6a56934adc
fc9 <fabiocabralsantos@gmail.com>
2020-01-01 04:52:58 -0300

    Versao testada

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,5 @@
 Vagrant.configure("2") do |config|
-    config.vm.box = "ubuntu-server-18.04-lts-amd64-bionic-beaver"
-    #config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
-    #config.vm.box_url = "https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-vagrant.box"
+    config.vm.box = "ubuntu-18.04-lts-amd64"
     config.vm.box_url = "https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64-vagrant.box"
     config.vm.network :forwarded_port, guest: 80, host: 8080
     config.vm.network :private_network, ip: "192.168.33.9"
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "---- Iniciando instalacao do ambiente de Desenvolvimento ---"
 
 echo "--- Atualizando lista de pacotes disponiveis ---"
-sudo apt-get -s update && apt-get dist-upgrade
+sudo apt-get update && apt-get upgrade --assume-yes
 
 echo "--- Definindo Senha padrao para o MySQL e suas ferramentas ---"
 
@@ -28,11 +28,12 @@ echo "--- Instalando pacotes basicos ---"
 sudo apt-get install vim curl python-software-properties git-core --assume-yes
 
 echo "--- Adicionando Apache 2 ---"
-sudo apt-get install apache2
+sudo apt-get install apache2 --assume-yes
+sudo apt-get upgrade --assume-yes
 
 echo "--- Instalando MySQL, Phpmyadmin e alguns outros modulos ---"
 sudo apt-get install mysql-server mysql-client phpmyadmin --assume-yes
-mysql_secure_installation
+sudo mysql_secure_installation
 
 echo "--- Iniciando MySQL ---"
 sudo service mysql start
@@ -46,13 +47,17 @@ sudo apt-get install php libapache2-mod-php --assume-yes
 # ver os modulos PHP disponiveis
 # sudo apt-cachesearch php7.2
 
-echo "---- Instalando alguns modulos do PHP 7.2 ---"
-sudo apt-get install php7.2-curl php7.2-cli php7.2-common php7.2-tokenizer php7.2-mbstring php7.2-xml php7.2-mysql php7.2-json php7.2-soap php7.2-gd php7.2-gmp php7.2-xsl  php7.2-xmlwrite php7.2-xmlreader php7.2-wddx php7.2-sysvshm php7.2-sysvmsg php7.2-sysvsem php7.2-sockets php7.2-simplexml --assume-yes
+echo "--- Ativando o repositório Universe ----"
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+
+echo "--- Instalando alguns modulos do PHP 7.2 ---"
+sudo apt-get install php-zip
+sudo apt-get install php7.2-zip php7.2-curl php7.2-cli php7.2-common php7.2-tokenizer php7.2-mbstring php7.2-xml php7.2-mysql php7.2-json php7.2-soap php7.2-gd php7.2-gmp php7.2-xsl php7.2-xmlwrite php7.2-xmlreader php7.2-wddx php7.2-sysvshm php7.2-sysvmsg php7.2-sysvsem php7.2-sockets php7.2-simplexml --assume-yes
 
 echo "--- Habilitando mod-rewrite do Apache ---"
 sudo a2enmod rewrite
 
-echo "-- Desabilitando o modulo php7 e habilitando php7.2 ---"
+echo "-- Habilitando php7.2 ---"
 sudo a2enmod php7.2
 
 echo "--- Reiniciando Apache ---"
@@ -66,7 +71,7 @@ curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 echo "--- Criando arquivo teste ---"
-echo "<?php phpinfo();>" /var/www/html/info.php
+echo '<?php phpinfo();' > /var/www/html/info.php
 
 # Instale a partir daqui o que você desejar
 
